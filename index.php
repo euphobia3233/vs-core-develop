458a2d1e7a98ebc4bd1b697987b8ca7b74ac2af0
euphobia3233 <euphobia3233@gmail.com>
2014-08-24 00:27:21 +0530

    after merge conflict resolve

--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
 echo "this is simple code";
-<<<<<<< HEAD
-
 echo "this is next line after branch creation";
-=======
 echo "this line is appended from github";
 echo "this line is added from branch second";
->>>>>>> feature/2-second
 ?>
